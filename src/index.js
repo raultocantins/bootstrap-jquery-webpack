cc95000117da096d2b4a3ebdda8eae9739893d60
Alex Raul Santo Araujo <06562980143@caltins.local>
2020-03-18 11:49:46 -0300

    Projeto de uma galeria utilizando Bootstrap-jquery e Tambem Webpack para fazer a build da aplicação.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// arquivo sass
+import './scss/index.scss'
+// dependencias 
+import 'jquery'
+import 'bootstrap'
+//meus arquivos js
+import './js/core/includes'
+import './js/plugins/cityButtons'
